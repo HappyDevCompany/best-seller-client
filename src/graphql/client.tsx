ec92f66c8824231fe27511a68065687e3584a9f0
happyDev <93234062+HappyDevCompany@users.noreply.github.com>
2022-11-07 13:43:58 +0100

    Update client.tsx

--- src/graphql/client.tsx
+++ src/graphql/client.tsx
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   //uri: "http://localhost:4000/graphql",
-  uri:"https://best-seller-server.herokuapp.com"
+  uri:"https://best-seller-server.herokuapp.com",
   cache: new InMemoryCache(),
 });
 
